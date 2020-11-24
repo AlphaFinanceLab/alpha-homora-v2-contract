8697a183cb3d154fe8223c36e91e920e6deb5292
Griphook <griphook.eth@protonmail.com>
2020-11-24 10:07:45 +0000

    More complete Homora ETH router

--- contracts/WETHRouter.sol
+++ contracts/WETHRouter.sol
@@ -8,23 +8,29 @@ import '../interfaces/IWETH.sol';
 contract HomoraETHRouter {
   IBank public bank;
   IWETH public weth;
-  IERC20 public homo;
+  IERC20 public ib;
 
+  /// @dev Create a new Homora ETH router smart contract.
+  /// @param _bank The Homora bank smart contract.
+  /// @param _weth The wrapped ETH smart contract.
   constructor(IBank _bank, IWETH _weth) public {
     bank = _bank;
     weth = _weth;
-    // TODO: homo = ...
+    ib = IERC20(bank.ibTokenOf(address(weth)));
     weth.approve(address(_bank), uint(-1));
+    ib.approve(address(_bank), uint(-1));
   }
 
-  // TODO
+  /// @dev Deposit ETH into Homora Bank and return back ibETH to the caller.
   function deposit() public payable {
     weth.deposit{value: msg.value}();
     bank.deposit(address(weth), msg.value);
-    homo.transfer(msg.sender, homo.balanceOf(address(this)));
+    ib.transfer(msg.sender, ib.balanceOf(address(this)));
   }
 
+  /// @dev Withdraw ibETH for the caller and return back the ETH.
   function withdraw(uint share) public {
+    ib.transferFrom(msg.sender, address(this), share);
     bank.withdraw(address(weth), share);
     uint value = weth.balanceOf(address(this));
     weth.withdraw(value);
