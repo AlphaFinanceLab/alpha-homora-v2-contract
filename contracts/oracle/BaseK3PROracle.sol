385f9f43f08cd3f4d9103b6d41d4d13f99a4248c
Griphook <griphook.eth@protonmail.com>
2020-11-27 14:20:53 +0000

    Remove unnessary code after integration with other lending platforms

--- contracts/oracle/BaseK3PROracle.sol
+++ contracts/oracle/BaseK3PROracle.sol
@@ -3,48 +3,6 @@ pragma solidity 0.6.12;
 import '../../interfaces/IKeep3rV1Oracle.sol';
 import '../../interfaces/IUniswapV2Pair.sol';
 
-// library UniswapV2Library {
-//   // returns sorted token addresses, used to handle return values from pairs sorted in this order
-//   function sortTokens(address tokenA, address tokenB)
-//     internal
-//     pure
-//     returns (address token0, address token1)
-//   {
-//     require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
-//     (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-//     require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
-//   }
-
-//   // calculates the CREATE2 address for a pair without making any external calls
-//   function pairInfo(
-//     address factory,
-//     address tokenA,
-//     address tokenB
-//   )
-//     internal
-//     pure
-//     returns (
-//       address pair,
-//       address token0,
-//       address token1
-//     )
-//   {
-//     (token0, token1) = sortTokens(tokenA, tokenB);
-//     pair = address(
-//       uint(
-//         keccak256(
-//           abi.encodePacked(
-//             hex'ff',
-//             factory,
-//             keccak256(abi.encodePacked(token0, token1)),
-//             hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
-//           )
-//         )
-//       )
-//     );
-//   }
-// }
-
 contract BaseK3PROracle {
   uint public constant MIN_TWAP_TIME = 15 minutes;
   uint public constant MAX_TWAP_TIME = 60 minutes;
