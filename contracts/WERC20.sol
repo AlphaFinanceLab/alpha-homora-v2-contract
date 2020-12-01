27a18408203128510aa465f80f3970dea06ea3c5
Griphook <griphook.eth@protonmail.com>
2020-12-01 17:12:08 +0000

    Add IWERC20 interface

--- contracts/WERC20.sol
+++ contracts/WERC20.sol
@@ -5,20 +5,27 @@ import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/IERC20.s
 import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/SafeERC20.sol';
 import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/utils/ReentrancyGuard.sol';
 
-contract WERC20 is ERC1155(''), ReentrancyGuard {
+import '../interfaces/IWERC20.sol';
+
+contract WERC20 is ERC1155(''), ReentrancyGuard, IWERC20 {
   using SafeERC20 for IERC20;
 
+  /// @dev Return the underlying ERC20 balance for the user.
+  function balanceOfERC20(address token, address user) external view override returns (uint) {
+    return balanceOf(user, uint(token));
+  }
+
   /// @dev Mint ERC1155 token for the given ERC20 token.
-  function mint(IERC20 token, uint amount) external nonReentrant {
-    uint balanceBefore = token.balanceOf(address(this));
-    token.safeTransferFrom(msg.sender, address(this), amount);
-    uint balanceAfter = token.balanceOf(address(this));
-    _mint(msg.sender, uint(address(token)), balanceAfter.sub(balanceBefore), '');
+  function mint(address token, uint amount) external override nonReentrant {
+    uint balanceBefore = IERC20(token).balanceOf(address(this));
+    IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+    uint balanceAfter = IERC20(token).balanceOf(address(this));
+    _mint(msg.sender, uint(token), balanceAfter.sub(balanceBefore), '');
   }
 
   /// @dev Burn ERC1155 token to redeem ERC20 token back.
-  function burn(IERC20 token, uint amount) external nonReentrant {
-    _burn(msg.sender, uint(address(token)), amount);
-    token.safeTransfer(msg.sender, amount);
+  function burn(address token, uint amount) external override nonReentrant {
+    _burn(msg.sender, uint(token), amount);
+    IERC20(token).safeTransfer(msg.sender, amount);
   }
 }
