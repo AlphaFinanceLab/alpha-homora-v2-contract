5514cb1db100174515c5b4b7e45f7a599bc50938
Griphook <griphook.eth@protonmail.com>
2020-12-01 17:38:35 +0000

    Add IWERC20 interface code

---
+++ interfaces/IWERC20.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.6.12;
+
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC1155/IERC1155.sol';
+
+interface IWERC20 is IERC1155 {
+  /// @dev Return the underlying ERC20 balance for the user.
+  function balanceOfERC20(address token, address user) external view returns (uint);
+
+  /// @dev Mint ERC1155 token for the given ERC20 token.
+  function mint(address token, uint amount) external;
+
+  /// @dev Burn ERC1155 token to redeem ERC20 token back.
+  function burn(address token, uint amount) external;
+}
