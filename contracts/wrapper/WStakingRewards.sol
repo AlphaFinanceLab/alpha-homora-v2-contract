77da4d18be2b937e017d2eb63b2ac22c2d2593eb
Griphook <griphook.eth@protonmail.com>
2020-12-03 07:45:37 +0000

    Add WStakingRewards

---
+++ contracts/wrapper/WStakingRewards.sol
@@ -0,0 +1,61 @@
+pragma solidity 0.6.12;
+
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC1155/ERC1155.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/IERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/SafeERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/utils/ReentrancyGuard.sol';
+
+import '../utils/HomoraMath.sol';
+import '../../interfaces/IERC20Wrapper.sol';
+import '../../interfaces/IStakingRewards.sol';
+
+contract WStakingRewards is ERC1155, ReentrancyGuard, IERC20Wrapper {
+  using SafeMath for uint;
+  using HomoraMath for uint;
+  using SafeERC20 for IERC20;
+
+  address public staking;
+  address public underlying;
+  address public reward;
+
+  constructor(
+    address _staking,
+    address _underlying,
+    address _reward
+  ) public ERC1155('TODO') {
+    staking = _staking;
+    underlying = _underlying;
+    reward = _reward;
+    IERC20(underlying).approve(staking, uint(-1));
+  }
+
+  function getUnderlying(uint) external view override returns (address) {
+    return underlying;
+  }
+
+  function mint(uint amount) external nonReentrant returns (uint) {
+    IERC20(underlying).safeTransferFrom(msg.sender, address(this), amount);
+    IStakingRewards(staking).stake(amount);
+    uint rewardPerToken = IStakingRewards(staking).rewardPerToken();
+    _mint(msg.sender, rewardPerToken, amount, '');
+    return rewardPerToken;
+  }
+
+  function burn(uint id, uint amount) external nonReentrant returns (uint) {
+    if (amount == uint(-1)) {
+      amount = balanceOf(msg.sender, id);
+    }
+    _burn(msg.sender, id, amount);
+    IStakingRewards(staking).withdraw(amount);
+    IStakingRewards(staking).getReward();
+    IERC20(underlying).safeTransfer(msg.sender, amount);
+    uint stRewardPerToken = id;
+    uint enRewardPerToken = IStakingRewards(staking).rewardPerToken();
+    uint stReward = stRewardPerToken.mul(amount).divCeil(1e18);
+    uint enReward = enRewardPerToken.mul(amount).div(1e18);
+    if (enReward > stReward) {
+      IERC20(reward).safeTransfer(msg.sender, enReward.sub(stReward));
+    }
+    return amount;
+  }
+}
