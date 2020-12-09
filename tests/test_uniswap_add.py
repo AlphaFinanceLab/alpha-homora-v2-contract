b55357ce2c1ff8ad4dea841a26824a67efe1ba32
n1punp <disney31215@gmail.com>
2020-12-09 17:42:02 +0000

    Update uniswap test

--- tests/test_uniswap_add.py
+++ tests/test_uniswap_add.py
@@ -1,8 +1,9 @@
 import pytest
+from brownie import interface
 
 
 def test_uniswap_add_two_tokens(
-    a, chain, bank, werc20, ufactory, urouter, simple_oracle, oracle, usdc, usdt, UniswapV2SpellV1,
+    a, chain, bank, werc20, ufactory, urouter, simple_oracle, oracle, usdc, usdt, UniswapV2SpellV1, UniswapLPOracle,
 ):
     spell = UniswapV2SpellV1.deploy(bank, werc20, urouter, {'from': a[0]})
     usdc.mint(a[0], 10000000 * 10**6, {'from': a[0]})
@@ -20,17 +21,21 @@ def test_uniswap_add_two_tokens(
         chain.time() + 60,
         {'from': a[0]},
     )
+
     lp = ufactory.getPair(usdc, usdt)
-    simple_oracle.setETHPx(
-        [lp],
-        [10**50],  # Way too much!
-    )
+    print('admin lp bal', interface.IERC20(lp).balanceOf(a[0]))
+    uniswap_lp_oracle = UniswapLPOracle.deploy(simple_oracle, {'from': a[0]})
+
+    print('usdt Px', simple_oracle.getETHPx(usdt))
+    print('usdc Px', simple_oracle.getETHPx(usdc))
+
+    print('lp Px', uniswap_lp_oracle.getETHPx(lp))
     oracle.setOracles(
         [usdc, usdt, lp],
         [
             [simple_oracle, 10000, 10000, 10000],
             [simple_oracle, 10000, 10000, 10000],
-            [simple_oracle, 10000, 10000, 10000],
+            [uniswap_lp_oracle, 10000, 10000, 10000],
         ],
         {'from': a[0]},
     )
@@ -53,9 +58,22 @@ def test_uniswap_add_two_tokens(
                 200 * 10**6,  # 200 USDC
                 0,  # borrow LP tokens
                 0,  # min USDT
-                0,
-            ],  # min USDC
+                0,  # min USDC
+            ],
         ),
         {'from': a[1]}
     )
-    assert 900000 <= tx.gas_used <= 1000000
+
+    position_id = tx.return_value
+    print('tx gas used', tx.gas_used)
+    print('bank collateral size', bank.getPositionInfo(position_id))
+    print('bank collateral value', bank.getCollateralETHValue(position_id))
+    print('bank borrow value', bank.getBorrowETHValue(position_id))
+
+    print('bank usdt', bank.getBankInfo(usdt))
+    print('bank usdc', bank.getBankInfo(usdc))
+
+    print('usdt Px', simple_oracle.getETHPx(usdt))
+    print('usdc Px', simple_oracle.getETHPx(usdc))
+
+    print('lp Px', uniswap_lp_oracle.getETHPx(lp))
