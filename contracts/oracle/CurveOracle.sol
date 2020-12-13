0275eb196bad69e6942703880bb9cc1d37f5ab97
n1punp <disney31215@gmail.com>
2020-12-13 08:45:40 +0000

    Fix curve spell and use registry in oracle

--- contracts/oracle/CurveOracle.sol
+++ contracts/oracle/CurveOracle.sol
@@ -4,6 +4,7 @@ import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/math/SafeMath.sol';
 
 import '../../interfaces/IBaseOracle.sol';
 import '../../interfaces/ICurvePool.sol';
+import '../../interfaces/ICurveRegistry.sol';
 
 interface IERC20Decimal {
   function decimals() external view returns (uint8);
@@ -13,26 +14,48 @@ contract CurveOracle is IBaseOracle {
   using SafeMath for uint;
 
   IBaseOracle public tokenOracle;
+  ICurveRegistry public registry;
 
-  constructor(IBaseOracle _tokenOracle) public {
+  mapping(address => address[]) public ulTokens; // lpToken -> underlying token array
+  mapping(address => address) public poolOf; // lpToken -> pool
+
+  constructor(IBaseOracle _tokenOracle, ICurveRegistry _registry) public {
     tokenOracle = _tokenOracle;
+    registry = _registry;
+  }
+
+  /// @dev Return pool address given LP token and update pool info if not exist.
+  /// @param lp LP token to find the corresponding pool.
+  function getPool(address lp) public returns (address) {
+    address pool = poolOf[lp];
+    if (pool == address(0)) {
+      require(lp != address(0), 'no lp token');
+      pool = registry.get_pool_from_lp_token(lp);
+      poolOf[lp] = pool;
+      uint n = registry.get_n_coins(pool);
+      address[8] memory tokens = registry.get_coins(pool);
+      ulTokens[lp] = new address[](n);
+      for (uint i = 0; i < n; i++) {
+        ulTokens[lp][i] = tokens[i];
+      }
+    }
+    return pool;
   }
 
   /// @dev Return the value of the given input as ETH per unit, multiplied by 2**112.
-  /// @param token The ERC-20 token to check the value.
-  function getETHPx(address token) external view override returns (uint) {
-    ICurvePool pool = ICurvePool(token);
+  /// @param lp The ERC-20 LP token to check the value.
+  function getETHPx(address lp) external view override returns (uint) {
+    ICurvePool pool = ICurvePool(poolOf[lp]);
     uint minPx = uint(-1);
-    for (uint idx = 0; ; idx++) {
-      try pool.coins(idx) returns (address coin) {
-        uint decimals = IERC20Decimal(coin).decimals();
-        uint tokenPx = tokenOracle.getETHPx(coin);
-        if (decimals < 18) tokenPx = tokenPx.div(10**(18 - decimals));
-        if (decimals > 18) tokenPx = tokenPx.mul(10**(decimals - 18));
-        if (tokenPx < minPx) minPx = tokenPx;
-      } catch (bytes memory) {
-        break;
-      }
+    address[] memory tokens = ulTokens[lp];
+    uint n = tokens.length;
+    for (uint idx = 0; idx < n; idx++) {
+      address token = tokens[idx];
+      uint decimals = IERC20Decimal(token).decimals();
+      uint tokenPx = tokenOracle.getETHPx(token);
+      if (decimals < 18) tokenPx = tokenPx.div(10**(18 - decimals));
+      if (decimals > 18) tokenPx = tokenPx.mul(10**(decimals - 18));
+      if (tokenPx < minPx) minPx = tokenPx;
     }
     require(minPx != uint(-1), 'no min px');
     return minPx.mul(pool.get_virtual_price()).div(1e18);
---
+++ scripts/curve_oracle_test.py
@@ -0,0 +1,95 @@
+from brownie import accounts, interface, Contract
+from brownie import (
+    HomoraBank, ProxyOracle, SimpleOracle, CurveOracle, CurveSpellV1, WERC20
+)
+
+
+def almostEqual(a, b):
+    thresh = 0.01
+    return a <= b + thresh * abs(b) and a >= b - thresh * abs(b)
+
+
+# add collateral for the bank
+def setup_bank_hack(homora):
+    donator = accounts[5]
+    fake = accounts.at(homora.address, force=True)
+    controller = interface.IComptroller(
+        '0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258')
+    creth = interface.ICEtherEx('0xD06527D5e56A3495252A528C4987003b712860eE')
+    creth.mint({'value': '90 ether', 'from': donator})
+    creth.transfer(fake, creth.balanceOf(donator), {'from': donator})
+    controller.enterMarkets([creth], {'from': fake})
+
+
+def setup_transfer(asset, fro, to, amt):
+    print(f'sending from {fro} {amt} {asset.name()} to {to}')
+    asset.transfer(to, amt, {'from': fro})
+
+
+def main():
+    admin = accounts[0]
+
+    alice = accounts[1]
+    dai = interface.IERC20Ex('0x6B175474E89094C44Da98b954EedeAC495271d0F')
+    usdc = interface.IERC20Ex('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
+    usdt = interface.IERC20Ex('0xdAC17F958D2ee523a2206206994597C13D831ec7')
+
+    lp = interface.IERC20Ex('0x6c3f90f043a72fa612cbac8115ee7e52bde6e490')
+    pool = interface.ICurvePool('0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7')
+    registry = interface.ICurveRegistry(
+        '0x7d86446ddb609ed0f5f8684acf30380a356b2b4c')
+
+    crdai = interface.ICErc20('0x92B767185fB3B04F881e3aC8e5B0662a027A1D9f')
+    crusdc = interface.ICErc20('0x44fbebd2f576670a6c33f6fc0b00aa8c5753b322')
+    crusdt = interface.ICErc20('0x797AAB1ce7c01eB727ab980762bA88e7133d2157')
+
+    werc20 = WERC20.deploy({'from': admin})
+
+    simple_oracle = SimpleOracle.deploy({'from': admin})
+    simple_oracle.setETHPx([dai, usdt, usdc], [9060553589188986552095106856227,
+                                               9002288773315920458132820329673073223442669,
+                                               9011535487953795006625883219171279625142296])
+
+    curve_oracle = CurveOracle.deploy(simple_oracle, registry, {'from': admin})
+    curve_oracle.getPool(lp)  # update pool info
+
+    oracle = ProxyOracle.deploy({'from': admin})
+    oracle.setWhitelistERC1155([werc20], True, {'from': admin})
+    oracle.setOracles(
+        [
+            '0x6B175474E89094C44Da98b954EedeAC495271d0F',  # DAI
+            '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',  # USDC
+            '0xdAC17F958D2ee523a2206206994597C13D831ec7',  # USDT
+            '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490',  # lp
+        ],
+        [
+            [simple_oracle, 10000, 10000, 10000],
+            [simple_oracle, 10000, 10000, 10000],
+            [simple_oracle, 10000, 10000, 10000],
+            [curve_oracle, 10000, 10000, 10000],
+        ],
+        {'from': admin},
+    )
+
+    #####################################################################################
+
+    print('=========================================================================')
+    print('Case 1.')
+
+    tx = curve_oracle.getPool(lp)
+    print('pool addr', tx.return_value)
+    print('pool virtual price', pool.get_virtual_price())
+
+    lp_price = curve_oracle.getETHPx(lp)
+    dai_price = simple_oracle.getETHPx(dai)
+    usdt_price = simple_oracle.getETHPx(usdt)
+    usdc_price = simple_oracle.getETHPx(usdc)
+
+    print('lp price', lp_price)
+    print('dai price', dai_price)
+    print('usdt price', usdt_price)
+    print('usdc price', usdc_price)
+
+    # min price is from USDT
+    assert almostEqual(9002288773315920458132820329673073223442669 *
+                       pool.get_virtual_price() * 10 ** 6 // 10**18 // 10 ** 18, lp_price)
