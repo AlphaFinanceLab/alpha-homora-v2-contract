4e9f9aa43fdde71cbc1b34d95be4e7ff271b0a68
n1punp <disney31215@gmail.com>
2021-01-04 17:05:49 +0000

    Add collateral token require check with test to check revert

--- scripts/sushiswap_spell_add_remove_test.py
+++ scripts/sushiswap_spell_add_remove_test.py
@@ -1,9 +1,7 @@
-from brownie import accounts, interface, Contract, chain
+from brownie import accounts, interface, Contract
 from brownie import (
-    HomoraBank, ProxyOracle, UniswapV2Oracle, SimpleOracle, SushiswapSpellV1, WERC20, WMasterChef, MockCErc20
+    HomoraBank, ProxyOracle, UniswapV2Oracle, SimpleOracle, SushiswapSpellV1, WERC20, WMasterChef
 )
-import brownie
-from brownie.exceptions import VirtualMachineError
 
 
 def almostEqual(a, b):
@@ -31,19 +29,12 @@ def main():
     admin = accounts[0]
 
     alice = accounts[1]
-    bob = accounts[2]
-    usdt = interface.IERC20Ex('0xdac17f958d2ee523a2206206994597c13d831ec7')
+    usdt = interface.IERC20Ex('0xdAC17F958D2ee523a2206206994597C13D831ec7')
     weth = interface.IERC20Ex('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
-    usdc = interface.IERC20Ex('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
 
     lp = interface.IERC20Ex('0x06da0fd433C1A5d7a4faa01111c044910A184553')
-    lp_usdc = interface.IERC20Ex('0x3041cbd36888becc7bbcbc0045e3b1f144466f5f')
     crusdt = interface.ICErc20('0x797AAB1ce7c01eB727ab980762bA88e7133d2157')
-    crusdc = interface.ICErc20('0x44fbebd2f576670a6c33f6fc0b00aa8c5753b322')
 
-    sushi = interface.IERC20('0x6b3595068778dd592e39a122f4f5a5cf09c90fe2')
-
-    # sushiswap router
     router = interface.IUniswapV2Router02(
         '0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f')
 
@@ -54,23 +45,19 @@ def main():
     werc20 = WERC20.deploy({'from': admin})
 
     simple_oracle = SimpleOracle.deploy({'from': admin})
-    simple_oracle.setETHPx([usdt, weth, usdc], [2**112 // 700, 2**112, 2**112 // 700])
+    simple_oracle.setETHPx([usdt, weth], [2**112 // 700, 2**112])
 
     uniswap_oracle = UniswapV2Oracle.deploy(simple_oracle, {'from': admin})
     oracle = ProxyOracle.deploy({'from': admin})
     oracle.setWhitelistERC1155([werc20, wchef], True, {'from': admin})
     oracle.setOracles(
         [
-            '0xdac17f958d2ee523a2206206994597c13d831ec7',  # USDT
+            '0xdAC17F958D2ee523a2206206994597C13D831ec7',  # USDT
             '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',  # WETH
             '0x06da0fd433C1A5d7a4faa01111c044910A184553',  # USDT-WETH
-            '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',  # USDC
-            '0x397ff1542f962076d0bfe58ea045ffa2d347aca0',  # USDC-WETH
         ],
         [
             [simple_oracle, 10000, 10000, 10000],
-            [simple_oracle, 10000, 10000, 10000],
-            [uniswap_oracle, 10000, 10000, 10000],
             [simple_oracle, 10000, 10000, 10000],
             [uniswap_oracle, 10000, 10000, 10000],
         ],
@@ -81,42 +68,35 @@ def main():
     homora.initialize(oracle, 1000, {'from': admin})  # 10% fee
     setup_bank_hack(homora)
     homora.addBank(usdt, crusdt, {'from': admin})
-    homora.addBank(usdc, crusdc, {'from': admin})
 
     # setup initial funds 10^5 USDT + 10^4 WETH to alice
     setup_transfer(usdt, accounts.at(
         '0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503', force=True), alice, 10**5 * 10**6)
     setup_transfer(weth, accounts.at(
         '0x397ff1542f962076d0bfe58ea045ffa2d347aca0', force=True), alice, 10**4 * 10**18)
-    setup_transfer(usdc, accounts.at('0xa191e578a6736167326d05c119ce0c90849e84b7',
-                                     force=True), alice, 10**6 * 10**6)
 
-    # setup initial funds 10^5 USDT + 10^5 WETH to homora bank
+    # setup initial funds 10^6 USDT + 10^5 WETH to homora bank
     setup_transfer(usdt, accounts.at(
-        '0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503', force=True), homora, 10**5 * 10**6)
+        '0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503', force=True), homora, 10**6 * 10**6)
     setup_transfer(weth, accounts.at(
         '0x397ff1542f962076d0bfe58ea045ffa2d347aca0', force=True), homora, 10**4 * 10**18)
-    setup_transfer(usdc, accounts.at('0xa191e578a6736167326d05c119ce0c90849e84b7',
-                                     force=True), homora, 10**6 * 10**6)
+
+    # Steal some LP from the staking pool
+    lp.transfer(alice, 4 * 10**13, {'from': accounts.at(
+        '0x1f9ff03e89e64138355fef8dcb45d7789a3d407a', force=True)})
+    lp.transfer(homora, 4 * 10**13, {'from': accounts.at(
+        '0x1f9ff03e89e64138355fef8dcb45d7789a3d407a', force=True)})
 
     # check alice's funds
     print(f'Alice usdt balance {usdt.balanceOf(alice)}')
     print(f'Alice weth balance {weth.balanceOf(alice)}')
-
-    # Steal some LP from the staking pool
-    lp.transfer(alice, 4*10**13, {'from': accounts.at(
-        '0x1f9ff03e89e64138355fef8dcb45d7789a3d407a', force=True)})
-    lp.transfer(bob, 4*10**13, {'from': accounts.at(
-        '0x1f9ff03e89e64138355fef8dcb45d7789a3d407a', force=True)})
+    print(f'Alice weth balance {lp.balanceOf(alice)}')
 
     # set approval
     usdt.approve(homora, 2**256-1, {'from': alice})
     usdt.approve(crusdt, 2**256-1, {'from': alice})
-    usdc.approve(homora, 2**256-1, {'from': alice})
-    usdc.approve(crusdc, 2**256-1, {'from': alice})
     weth.approve(homora, 2**256-1, {'from': alice})
     lp.approve(homora, 2**256-1, {'from': alice})
-    lp.approve(chef, 2**256-1, {'from': bob})
 
     sushiswap_spell = SushiswapSpellV1.deploy(
         homora, werc20, router, wchef, {'from': admin})
@@ -125,31 +105,29 @@ def main():
 
     #####################################################################################
     print('=========================================================================')
-    print('Case 1. add liquidity first time')
+    print('Case 1.')
 
     prevABal = usdt.balanceOf(alice)
     prevBBal = weth.balanceOf(alice)
     prevLPBal = lp.balanceOf(alice)
     prevLPBal_bank = lp.balanceOf(homora)
-    prevLPBal_chef = lp.balanceOf(chef)
+    prevLPBal_werc20 = lp.balanceOf(werc20)
 
     if interface.IUniswapV2Pair(lp).token0() == usdt:
         prevARes, prevBRes, _ = interface.IUniswapV2Pair(lp).getReserves()
     else:
         prevBRes, prevARes, _ = interface.IUniswapV2Pair(lp).getReserves()
 
-    usdt_amt = 10 * 10**6
-    weth_amt = 10**18
-    lp_amt = 0
-    borrow_usdt_amt = 0
+    usdt_amt = 400 * 10**6
+    weth_amt = 10 ** 18
+    lp_amt = 1 * 10**13
+    borrow_usdt_amt = 10 * 10**6
     borrow_weth_amt = 0
-
-    pid = 0
 
     tx = homora.execute(
         0,
         sushiswap_spell,
-        sushiswap_spell.addLiquidityWMasterChef.encode_input(
+        sushiswap_spell.addLiquidityWERC20.encode_input(
             usdt,  # token 0
             weth,  # token 1
             [usdt_amt,  # supply USDT
@@ -160,7 +138,6 @@ def main():
              0,  # borrow LP tokens
              0,  # min USDT
              0],  # min WETH
-            pid,
         ),
         {'from': alice}
     )
@@ -172,7 +149,7 @@ def main():
     curBBal = weth.balanceOf(alice)
     curLPBal = lp.balanceOf(alice)
     curLPBal_bank = lp.balanceOf(homora)
-    curLPBal_chef = lp.balanceOf(chef)
+    curLPBal_werc20 = lp.balanceOf(werc20)
 
     if interface.IUniswapV2Pair(lp).token0() == usdt:
         curARes, curBRes, _ = interface.IUniswapV2Pair(lp).getReserves()
@@ -192,8 +169,8 @@ def main():
     print('bank prev LP balance', prevLPBal_bank)
     print('bank cur LP balance', curLPBal_bank)
 
-    print('chef prev LP balance', prevLPBal_chef)
-    print('chef cur LP balance', curLPBal_chef)
+    print('werc20 prev LP balance', prevLPBal_werc20)
+    print('werc20 cur LP balance', curLPBal_werc20)
 
     print('prev usdt res', prevARes)
     print('cur usdt res', curARes)
@@ -219,93 +196,17 @@ def main():
                        (curBRes - prevBRes)), 'not all WETH tokens go to LP pool'
 
     _, _, collId, collSize = homora.getPositionInfo(1)
-    print('collSize', collSize)
-
-    # staking directly
-    prevSushi = sushi.balanceOf(bob)
-    pid = 0
-    print('bob lp balance', interface.IERC20Ex(lp).balanceOf(bob))
-    tx = interface.IMasterChef(chef).deposit(pid, collSize, {'from': bob})
-
-    chain.sleep(20000)
-
-    prevAliceSushiBalance = sushi.balanceOf(alice)
-    print('Alice sushi balance', prevAliceSushiBalance)
 
     #####################################################################################
     print('=========================================================================')
-    print('Case 2. add liquidity (failed tx desired)')
+    print('Case 2.')
 
-    usdc_amt = 10 * 10**6
-    weth_amt = 10**18
-    lp_amt = 0
-    borrow_usdc_amt = 0
-    borrow_weth_amt = 0
-
-    pid = 0
-
-    try:
-        tx = homora.execute(
-            1,
-            sushiswap_spell,
-            sushiswap_spell.addLiquidityWMasterChef.encode_input(
-                usdc,  # token 0
-                weth,  # token 1
-                [usdc_amt,  # supply USDC
-                 weth_amt,   # supply WETH
-                 lp_amt,  # supply LP
-                 borrow_usdc_amt,  # borrow USDC
-                 borrow_weth_amt,  # borrow WETH
-                 0,  # borrow LP tokens
-                 0,  # min USDC
-                 0],  # min WETH
-                pid,
-            ),
-            {'from': alice}
-        )
-        assert False, 'tx not fail'
-    except VirtualMachineError:
-        pass
-
-    #####################################################################################
-    print('=========================================================================')
-    print('Case 3. remove liquidity (failed tx desired)')
-
-    lp_take_amt = collSize
-    lp_want = 0
-    usdc_repay = 0
-    weth_repay = 0
-
-    try:
-        tx = homora.execute(
-            1,
-            sushiswap_spell,
-            sushiswap_spell.removeLiquidityWMasterChef.encode_input(
-                usdc,  # token 0
-                weth,  # token 1
-                [lp_take_amt,  # take out LP tokens
-                 lp_want,   # withdraw LP tokens to wallet
-                 usdc_repay,  # repay USDC
-                 weth_repay,  # repay WETH
-                 0,  # repay LP tokens
-                 0,  # min USDC
-                 0],  # min WETH
-            ),
-            {'from': alice}
-        )
-        assert False, 'tx not failed'
-    except VirtualMachineError:
-        pass
-
-    #####################################################################################
-    print('=========================================================================')
-    print('Case 4. remove liquidity')
-
+    # remove liquidity from the same position
     prevABal = usdt.balanceOf(alice)
     prevBBal = weth.balanceOf(alice)
     prevLPBal = lp.balanceOf(alice)
     prevLPBal_bank = lp.balanceOf(homora)
-    prevLPBal_chef = lp.balanceOf(chef)
+    prevLPBal_werc20 = lp.balanceOf(werc20)
     prevETHBal = alice.balance()
 
     if interface.IUniswapV2Pair(lp).token0() == usdt:
@@ -314,37 +215,34 @@ def main():
         prevBRes, prevARes, _ = interface.IUniswapV2Pair(lp).getReserves()
 
     lp_take_amt = collSize
-    lp_want = 0
-    usdt_repay = 0
+    lp_want = 1 * 10**12
+    usdt_repay = 2**256-1  # max
     weth_repay = 0
 
-    pid = 0
+    real_usdt_repay = homora.borrowBalanceStored(position_id, usdt)
 
     tx = homora.execute(
-        1,
+        position_id,
         sushiswap_spell,
-        sushiswap_spell.removeLiquidityWMasterChef.encode_input(
+        sushiswap_spell.removeLiquidityWERC20.encode_input(
             usdt,  # token 0
             weth,  # token 1
             [lp_take_amt,  # take out LP tokens
              lp_want,   # withdraw LP tokens to wallet
              usdt_repay,  # repay USDT
-             weth_repay,  # repay WETH
-             0,  # repay LP tokens
-             0,  # min USDT
+             0,   # repay WETH
+             0,   # repay LP
+             0,   # min USDT
              0],  # min WETH
         ),
         {'from': alice}
     )
 
-    position_id = tx.return_value
-    print('position_id', position_id)
-
     curABal = usdt.balanceOf(alice)
     curBBal = weth.balanceOf(alice)
     curLPBal = lp.balanceOf(alice)
     curLPBal_bank = lp.balanceOf(homora)
-    curLPBal_chef = lp.balanceOf(chef)
+    curLPBal_werc20 = lp.balanceOf(werc20)
     curETHBal = alice.balance()
 
     if interface.IUniswapV2Pair(lp).token0() == usdt:
@@ -353,34 +251,36 @@ def main():
         curBRes, curARes, _ = interface.IUniswapV2Pair(lp).getReserves()
 
     print('spell lp balance', lp.balanceOf(sushiswap_spell))
+    print('spell usdt balance', usdt.balanceOf(sushiswap_spell))
+    print('spell weth balance', weth.balanceOf(sushiswap_spell))
     print('Alice delta A balance', curABal - prevABal)
     print('Alice delta B balance', curBBal - prevBBal)
-    print('add liquidity gas', tx.gas_used)
-    print('bank lp balance', curLPBal_bank)
+    print('Alice delta LP balance', curLPBal - prevLPBal)
+    print('remove liquidity gas', tx.gas_used)
+    print('bank delta lp balance', curLPBal_bank - prevLPBal_bank)
+    print('bank total lp balance', curLPBal_bank)
 
     _, _, _, totalDebt, totalShare = homora.getBankInfo(usdt)
     print('bank usdt totalDebt', totalDebt)
     print('bank usdt totalShare', totalShare)
 
-    print('bank prev LP balance', prevLPBal_bank)
-    print('bank cur LP balance', curLPBal_bank)
+    print('LP want', lp_want)
 
-    print('chef prev LP balance', prevLPBal_chef)
-    print('chef cur LP balance', curLPBal_chef)
+    print('bank delta LP amount', curLPBal_bank - prevLPBal_bank)
+    print('LP take amount', lp_take_amt)
 
-    print('prev usdt res', prevARes)
-    print('cur usdt res', curARes)
+    print('prev werc20 LP balance', prevLPBal_werc20)
+    print('cur werc20 LP balance', curLPBal_werc20)
 
-    print('prev weth res', prevBRes)
-    print('cur weth res', curBRes)
+    print('real usdt repay', real_usdt_repay)
 
     # alice
     assert almostEqual(curBBal - prevBBal, 0), 'incorrect WETH amt'
     assert almostEqual(curLPBal - prevLPBal, lp_want), 'incorrect LP amt'
 
-    # chef
-    assert almostEqual(curLPBal_chef - prevLPBal_chef, -
-                       lp_take_amt), 'incorrect chef LP amt'
+    # werc20
+    assert almostEqual(curLPBal_werc20 - prevLPBal_werc20, -
+                       lp_take_amt), 'incorrect werc20 LP amt'
 
     # spell
     assert usdt.balanceOf(sushiswap_spell) == 0, 'non-zero spell USDT balance'
@@ -388,21 +288,11 @@ def main():
     assert lp.balanceOf(sushiswap_spell) == 0, 'non-zero spell LP balance'
 
     # check balance and pool reserves
-    assert almostEqual(curABal - prevABal + usdt_repay, -
+    assert almostEqual(curABal - prevABal + real_usdt_repay, -
                        (curARes - prevARes)), 'inconsistent USDT from withdraw'
     assert almostEqual(curBBal - prevBBal,
                        0), 'inconsistent WETH from withdraw'
     assert almostEqual(curETHBal - prevETHBal + weth_repay, -
                        (curBRes - prevBRes)), 'inconsistent ETH from withdraw'
 
-    curAliceSushiBalance = sushi.balanceOf(alice)
-    print('Alice sushi balance', curAliceSushiBalance)
-    receivedSushi = curAliceSushiBalance - prevAliceSushiBalance
-    print('received sushi', receivedSushi)
-
-    # check with staking directly
-    pid = 0
-    tx = interface.IMasterChef(chef).withdraw(pid, collSize, {'from': bob})
-    receivedSushiFromStaking = sushi.balanceOf(bob) - prevSushi
-    print('receivedSushiFromStaking', receivedSushiFromStaking)
-    assert almostEqual(receivedSushi, receivedSushiFromStaking)
+    return tx
