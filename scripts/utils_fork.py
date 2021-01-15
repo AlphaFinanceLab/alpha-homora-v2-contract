4eda83537afbaad955c737a9b723db433b715a88
n1punp <disney31215@gmail.com>
2021-01-15 15:58:22 +0700

    Add utils_fork for mainnet fork

--- scripts/utils_fork.py
+++ scripts/utils_fork.py
@@ -4,7 +4,6 @@ try:
 except:
     pass
 
-
 USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7'
 USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 DAI = '0x6b175474e89094c44da98b954eedeac495271d0f'
