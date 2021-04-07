6df5a0fd9fdf1c57a1da92d9320f35d5260c4496
Nipun Pitimanaaree <disney31215@gmail.com>
2021-04-08 00:26:05 +0700

    Fix N-14

--- contracts/spell/SushiswapSpellV1.sol
+++ contracts/spell/SushiswapSpellV1.sol
@@ -152,14 +152,23 @@ contract SushiswapSpellV1 is WhitelistSpell {
     if (swapAmt > 0) {
       address[] memory path = new address[](2);
       (path[0], path[1]) = isReversed ? (tokenB, tokenA) : (tokenA, tokenB);
-      router.swapExactTokensForTokens(swapAmt, 0, path, address(this), now);
+      router.swapExactTokensForTokens(swapAmt, 0, path, address(this), block.timestamp);
     }
 
     // 5. Add liquidity
     uint balA = IERC20(tokenA).balanceOf(address(this));
     uint balB = IERC20(tokenB).balanceOf(address(this));
     if (balA > 0 || balB > 0) {
-      router.addLiquidity(tokenA, tokenB, balA, balB, amt.amtAMin, amt.amtBMin, address(this), now);
+      router.addLiquidity(
+        tokenA,
+        tokenB,
+        balA,
+        balB,
+        amt.amtAMin,
+        amt.amtBMin,
+        address(this),
+        block.timestamp
+      );
     }
   }
 
@@ -280,7 +289,7 @@ contract SushiswapSpellV1 is WhitelistSpell {
         0,
         0,
         address(this),
-        now
+        block.timestamp
       );
     }
 
@@ -296,7 +305,7 @@ contract SushiswapSpellV1 is WhitelistSpell {
         amtB.sub(amtBDesired),
         path,
         address(this),
-        now
+        block.timestamp
       );
     } else if (amtA >= amtADesired && amtB < amtBDesired) {
       address[] memory path = new address[](2);
@@ -306,7 +315,7 @@ contract SushiswapSpellV1 is WhitelistSpell {
         amtA.sub(amtADesired),
         path,
         address(this),
-        now
+        block.timestamp
       );
     }
 
--- contracts/spell/UniswapV2SpellV1.sol
+++ contracts/spell/UniswapV2SpellV1.sol
@@ -144,14 +144,23 @@ contract UniswapV2SpellV1 is WhitelistSpell {
     if (swapAmt > 0) {
       address[] memory path = new address[](2);
       (path[0], path[1]) = isReversed ? (tokenB, tokenA) : (tokenA, tokenB);
-      router.swapExactTokensForTokens(swapAmt, 0, path, address(this), now);
+      router.swapExactTokensForTokens(swapAmt, 0, path, address(this), block.timestamp);
     }
 
     // 5. Add liquidity
     uint balA = IERC20(tokenA).balanceOf(address(this));
     uint balB = IERC20(tokenB).balanceOf(address(this));
     if (balA > 0 || balB > 0) {
-      router.addLiquidity(tokenA, tokenB, balA, balB, amt.amtAMin, amt.amtBMin, address(this), now);
+      router.addLiquidity(
+        tokenA,
+        tokenB,
+        balA,
+        balB,
+        amt.amtAMin,
+        amt.amtBMin,
+        address(this),
+        block.timestamp
+      );
     }
   }
 
@@ -273,7 +282,7 @@ contract UniswapV2SpellV1 is WhitelistSpell {
         0,
         0,
         address(this),
-        now
+        block.timestamp
       );
     }
 
@@ -289,7 +298,7 @@ contract UniswapV2SpellV1 is WhitelistSpell {
         amtB.sub(amtBDesired),
         path,
         address(this),
-        now
+        block.timestamp
       );
     } else if (amtA >= amtADesired && amtB < amtBDesired) {
       address[] memory path = new address[](2);
@@ -299,7 +308,7 @@ contract UniswapV2SpellV1 is WhitelistSpell {
         amtA.sub(amtADesired),
         path,
         address(this),
-        now
+        block.timestamp
       );
     }
 
