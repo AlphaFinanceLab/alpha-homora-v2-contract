6892d129a60eed481bf63f5a9bc145494fb5042d
Nipun Pitimanaaree <disney31215@gmail.com>
2021-04-25 19:22:02 +0700

    Remove unused var

--- contracts/utils/ERC1155NaiveReceiver.sol
+++ contracts/utils/ERC1155NaiveReceiver.sol
@@ -8,21 +8,21 @@ contract ERC1155NaiveReceiver is ERC1155Receiver {
   bytes32[64] __gap; // reserve space for upgrade
 
   function onERC1155Received(
-    address operator,
-    address from,
-    uint id,
-    uint value,
-    bytes calldata data
+    address, /* operator */
+    address, /* from */
+    uint, /* id */
+    uint, /* value */
+    bytes calldata /* data */
   ) external override returns (bytes4) {
     return this.onERC1155Received.selector;
   }
 
   function onERC1155BatchReceived(
-    address operator,
-    address from,
-    uint[] calldata ids,
-    uint[] calldata values,
-    bytes calldata data
+    address, /* operator */
+    address, /* from */
+    uint[] calldata, /* ids */
+    uint[] calldata, /* values */
+    bytes calldata /* data */
   ) external override returns (bytes4) {
     return this.onERC1155BatchReceived.selector;
   }
