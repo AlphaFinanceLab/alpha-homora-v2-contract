fe0de394e676a5ec5dcef8a6594c76330adb629e
Nipun Pitimanaaree <disney31215@gmail.com>
2021-04-27 12:38:08 +0700

    Update contract

--- contracts/HomoraBankV2LiquidateRouter.sol
+++ contracts/HomoraBankV2LiquidateRouter.sol
@@ -1,11 +1,12 @@
 pragma solidity 0.6.12;
 
-import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/IERC20.sol';
-import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/SafeERC20.sol';
-import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC1155/IERC1155.sol';
-import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC1155/ERC1155.sol';
-import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/math/SafeMath.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC20/IERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC20/SafeERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC1155/IERC1155.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC1155/ERC1155.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/math/SafeMath.sol';
 
+import './Governable.sol';
 import '../interfaces/IBank.sol';
 import '../interfaces/IWERC20.sol';
 import '../interfaces/IWStakingRewards.sol';
@@ -16,8 +17,18 @@ interface IWrapper {
   function burn(uint, uint) external;
 }
 
-contract HomoraBankV2LiquidateRouter {
+library LiquidateRouterSafeMath {
   using SafeMath for uint;
+
+  /// @dev Return round-up division
+  function ceilDiv(uint a, uint b) internal pure returns (uint) {
+    return a.add(b).sub(1).div(b);
+  }
+}
+
+contract HomoraBankV2LiquidateRouter is Governable {
+  using SafeMath for uint;
+  using LiquidateRouterSafeMath for uint;
   using SafeERC20 for IERC20;
 
   IBank bank;
@@ -25,9 +36,13 @@ contract HomoraBankV2LiquidateRouter {
   mapping(address => mapping(address => bool)) approved;
 
   constructor(IBank _bank) public {
+    __Governable__init();
     bank = _bank;
   }
 
+  /// @dev Ensure the target is approved
+  /// @param token Token to approve
+  /// @param spender Target address to approve to
   function ensureApprove(address token, address spender) public {
     if (!approved[token][spender]) {
       IERC20(token).safeApprove(spender, uint(-1));
@@ -35,16 +50,22 @@ contract HomoraBankV2LiquidateRouter {
     }
   }
 
+  /// @dev Liquidate Homora Bank's position and unwrap ERC1155 to ERC-20 LP token
+  /// @param positionId Position id to liquidate
+  /// @param debtToken Debt token to repay on behalf of the position owner
+  /// @param amountCall Amount of debt token to repay (uint(-1) to repay all)
+  /// @param minLPOut Min LP amount out (slippage control)
   function liquidate(
     uint positionId,
     address debtToken,
-    uint amountCall
+    uint amountCall,
+    uint minLPOut
   ) external {
     bank.accrue(debtToken);
     (, , , uint totalDebt, uint totalShare) = bank.getBankInfo(debtToken);
     (, address collToken, uint collId, ) = bank.getPositionInfo(positionId);
     uint oldShare = bank.getPositionDebtShareOf(positionId, debtToken);
-    uint oldDebt = oldShare.mul(totalDebt).div(totalShare);
+    uint oldDebt = oldShare.mul(totalDebt).ceilDiv(totalShare);
 
     if (amountCall == uint(-1)) {
       amountCall = oldDebt;
@@ -61,14 +82,15 @@ contract HomoraBankV2LiquidateRouter {
         address(collId),
         IERC1155(collToken).balanceOf(address(this), collId)
       );
-      IERC20(address(collId)).safeTransfer(
-        msg.sender,
-        IERC20(address(collId)).balanceOf(address(this))
-      );
+      uint lpBalance = IERC20(address(collId)).balanceOf(address(this));
+      require(lpBalance >= minLPOut, 'Actual LP amount out too low!');
+      IERC20(address(collId)).safeTransfer(msg.sender, lpBalance);
     } else {
       IWrapper(collToken).burn(collId, IERC1155(collToken).balanceOf(address(this), collId));
       address lp = IERC20Wrapper(collToken).getUnderlyingToken(collId);
-      IERC20(lp).safeTransfer(msg.sender, IERC20(lp).balanceOf(address(this)));
+      uint lpBalance = IERC20(lp).balanceOf(address(this));
+      require(lpBalance >= minLPOut, 'Actual LP amount out too low!');
+      IERC20(lp).safeTransfer(msg.sender, lpBalance);
 
       IERC20 rewardToken;
       if (target == keccak256(abi.encodePacked('WMasterChef'))) {
@@ -86,7 +108,34 @@ contract HomoraBankV2LiquidateRouter {
     }
   }
 
-  function onERC1155Received(address, address, uint, uint, bytes calldata) external view returns (bytes4) {
+  /// @dev Withdraw stuck ERC20 token
+  /// @param token Token to withdraw
+  function withdrawERC20(address token) external onlyGov {
+    require(msg.sender == governor, 'not gov');
+    uint balance = IERC20(token).balanceOf(address(this));
+    if (balance > 0) {
+      IERC20(token).safeTransfer(msg.sender, balance);
+    }
+  }
+
+  /// @dev Withdraw stuck ERC1155 token
+  /// @param token ERC1155 token
+  /// @param id ERC1155 token id
+  function withdrawERC1155(address token, uint id) external onlyGov {
+    require(msg.sender == governor, 'not gov');
+    uint balance = IERC1155(token).balanceOf(address(this), id);
+    if (balance > 0) {
+      IERC1155(token).safeTransferFrom(address(this), msg.sender, id, balance, new bytes(0));
+    }
+  }
+
+  function onERC1155Received(
+    address,
+    address,
+    uint,
+    uint,
+    bytes calldata
+  ) external view returns (bytes4) {
     return this.onERC1155Received.selector;
   }
 }
